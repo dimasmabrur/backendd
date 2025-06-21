57b70b3222b55efb455e21554c9e97b9543ba599
dimasmabrur <dimasmabrur15@gmail.com>
2025-06-21 10:34:14 +0700

    menambahkan data mobil

--- src/index.js
+++ src/index.js
@@ -22,3 +22,19 @@ app.get("/mobil", async (c) => {
     return c.text("Gagal mengambil data mobil: " + err.message, 500);
   }
 });
+
+// Tambah mobil
+app.post("/mobil", async (c) => {
+  try {
+    const { nama, harga, tersedia } = await c.req.json();
+    await c.env.DB.prepare(
+      "INSERT INTO mobil (nama, harga, tersedia) VALUES (?, ?, ?)"
+    )
+      .bind(nama, harga, tersedia)
+      .run();
+
+    return c.json({ message: "Mobil ditambahkan" }, 201);
+  } catch (err) {
+    return c.text("Gagal menambahkan mobil: " + err.message, 500);
+  }
+});
